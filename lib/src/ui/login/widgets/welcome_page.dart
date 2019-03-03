22aa18dd0c6bfce561d5ecf8d11ccec3ed13b5b2
Sudharshan <solderneer@gmail.com>
2019-03-03 17:41:22 +0800

    replaced pageview with nested navigator for better UX

--- lib/src/ui/login/widgets/welcome_page.dart
+++ lib/src/ui/login/widgets/welcome_page.dart
@@ -8,21 +8,24 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-      Spacer(),
-      SvgPicture.asset(welcomeSvg,
-          width: MediaQuery.of(context).size.width - 40.0),
-      Spacer(),
-      Text("Hi, there.", style: Theme.of(context).accentTextTheme.display4),
-      Padding(
-          padding: EdgeInsets.only(bottom: 20.0),
-          child: Text("Let's get you set up and ready to go!",
-              style: Theme.of(context).accentTextTheme.title,
-              textAlign: TextAlign.center)),
-      TextButton(
-          text: "Login",
-          onClickCallback: () => Navigator.pushNamed(context, "/login")),
-      TextButton(text: "Sign Up", onClickCallback: () => print("hello")),
-    ]);
+    return Padding(
+        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Spacer(),
+          SvgPicture.asset(welcomeSvg,
+              width: MediaQuery.of(context).size.width - 40.0),
+          Spacer(),
+          Text("Hi, there.", style: Theme.of(context).accentTextTheme.display4),
+          Padding(
+              padding: EdgeInsets.only(bottom: 20.0),
+              child: Text("Let's get you set up and ready to go!",
+                  style: Theme.of(context).accentTextTheme.title,
+                  textAlign: TextAlign.center)),
+          TextButton(
+              text: "Login",
+              onClickCallback: () =>
+                  Navigator.pushNamed(context, "welcome/login")),
+          TextButton(text: "Sign Up", onClickCallback: () => print("hello")),
+        ]));
   }
 }
