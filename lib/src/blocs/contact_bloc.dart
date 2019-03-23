da37fd3785a4e1d892c8f04cb65a53c007ba6309
Sudharshan <solderneer@gmail.com>
2019-03-23 21:23:05 +0800

    fix: removes resource class (fixes #30)

--- lib/src/blocs/contact_bloc.dart
+++ lib/src/blocs/contact_bloc.dart
@@ -1,17 +1,17 @@
 import "package:rxdart/rxdart.dart";
 
-import "../resources/repository.dart";
+import "../resources/contact_api_provider.dart";
 import "../models/user_model.dart";
 
 // TODO: SHOULD BE A INHERITED SCOPED BLOC Widget
 class ContactBloc {
-  final _repository = Repository();
+  final _provider = ContactApiProvider();
   final _contactsFetcher = PublishSubject<List<User>>();
 
   Observable<List<User>> get contacts => _contactsFetcher.stream;
 
   fetchContacts() async {
-    List<User> contactList = await _repository.fetchContacts();
+    List<User> contactList = await _provider.fetchContacts();
     _contactsFetcher.sink.add(contactList);
   }
 
