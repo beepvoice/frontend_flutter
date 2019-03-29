2a6b2c79ce96ad390da6c270ac84f434428aea63
Sudharshan <solderneer@gmail.com>
2019-03-29 10:33:45 +0800

    Revert "feat: using carthage eventsource and webrtc implementations"
    This reverts commit 184a0c0b540084341e0db0ffcf1557805cd453b6.

--- ios/Runner/RTCWrapper.swift
+++ ios/Runner/RTCWrapper.swift
@@ -10,26 +10,21 @@ import Foundation
 import WebRTC
 
 class RTCWrapper{
-    // Globals
-    var connectionFactory: RTCPeerConnectionFactory?
-    var peerConnection: RTCPeerConnection?
-    
-    // Constant configs
-    let defaultOfferConstraints = RTCMediaConstraints(mandatoryConstraints: ["OfferToReceiveAudio": "true", "OfferToReceiveVideo": "false"], optionalConstraints: nil)
-    let defaultConnectionConstraints = RTCMediaConstraints(mandatoryConstraints: nil, optionalConstraints: ["DtlsSrtpKeyAgreement": "true"])
+    var connectionFactory: RTCPeerConnectionFactory;
+    var peerConnection: RTCPeerConnection;
     
     init() {
         RTCPeerConnectionFactory.initialize()
         self.connectionFactory = RTCPeerConnectionFactory()
     }
     
-    func initialisePeerConnection() {
+    func initialisePeerConnection () {
         let configuration = RTCConfiguration()
         configuration.iceServers = self.iceServers
-        self.peerConnection = self.connectionFactory?.peerConnection(with: configuration, constraints: self.defaultConnectionConstraint, delegate: self)
+        self.peerConnection = self.connectionFactory.peerConnection(with: configuration, constraints: self.defaultConnectionConstraint, delegate: self)
     }
     
-    func startConnection() {
+    func startConnection () {
         guard let peerConnection = self.peerConnection else {
             return
         }
@@ -42,40 +37,5 @@ class RTCWrapper{
         }
     }
     
-    func localStream() -> RTCMediaStream {
-        if let factory = self.connectionFactory {
-            let localStream = factory.mediaStream(withStreamId: "RTCmS")
-            
-            if !AVCaptureState.isAudioDisabled {
-                let audioTrack = factory.audioTrack(withTrackId: "RTCaS0")
-                localStream.addAudioTrack(audioTrack)
-            } else {
-                // Show error for audio perms disabled
-                let error = NSError.init(domain: ErrorDomain.audioPermissionDenied, code: 0, userInfo: nil)
-                self.delegate?.rtcClient(client: self, didReceiveError: error)
-            }
-            
-            return localStream
-        }
-    }
     
-    func makeOffer() {
-        guard let peerConnection = self.peerConnection else {
-            return
-        }
-        
-        peerConnection.offer(for: self.defaultOffetConstraints, completionHandler: { (sdp, error) in
-            if let error = error {
-                self.delegate?.rtcClient(client: self, didReceiveError: error as NSError)
-            } else {
-                self.handleSdpGenerated(sdpDescription: sdp)
-            }
-        })
-    }
-    
-    func handleSdpGenerated(sdpDescription: RTCSessionDescription) {
-        guard let sdpDescription = sdpDescription else {
-            return
-        }
-    }
 }
