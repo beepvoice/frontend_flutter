a4dd1699a55e70b11e4b0ac1ceba0a6173b1770e
Sudharshan <solderneer@gmail.com>
2019-03-30 21:58:49 +0800

    feat: adding assets tto repo

--- ios/Runner/PeerManager.swift
+++ ios/Runner/PeerManager.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import WebRTC
+
+class PeerManager: NSObject {
+    // WebRTC initialization
+    var connectionFactory: RTCPeerConnectionFactory?
+    let signalingApiProvider: SignalingApiProvider
+    
+    // List of users
+    var peerList: [PeerConnectionWrapper] = []
+    
+    public override init() {
+        super.init()
+        initialisePeerConnectionFactory()
+    }
+    
+    public func join(conversationId: String, usersIds: [String]) {
+        
+    }
+    
+    public func exit() {
+        
+    }
+    
+    public func get() -> String {
+        
+    }
+}
+
+private extension PeerManager {
+    func initialisePeerConnectionFactory () {
+        RTCPeerConnectionFactory.initialize()
+        self.connectionFactory = RTCPeerConnectionFactory()
+    }
+}
