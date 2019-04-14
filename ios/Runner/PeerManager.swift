5f9c6a600559ff7436fbf6d69455926607b0c536
Sudharshan <solderneer@gmail.com>
2019-04-14 14:58:47 +0800

    feat: working on peer manager

--- ios/Runner/PeerManager.swift
+++ ios/Runner/PeerManager.swift
@@ -12,9 +12,10 @@ import WebRTC
 class PeerManager: NSObject {
     // WebRTC initialization
     var connectionFactory: RTCPeerConnectionFactory?
+    var eventSource: EventSource = EventSource(url: "https://staging.beepvoice.app/signal")
     
     // List of users
-    var peerList: [PeerConnectionWrapper] = []
+    var peerList: [String: PeerConnectionWrapper] = [:]
     
     public override init() {
         super.init()
@@ -35,8 +36,22 @@ class PeerManager: NSObject {
 }
 
 private extension PeerManager {
-    func initialisePeerConnectionFactory () {
+    func initialisePeerConnectionFactory() {
         RTCPeerConnectionFactory.initialize()
         self.connectionFactory = RTCPeerConnectionFactory()
+    }
+    
+    func initialiseEventSource() {
+        eventSource.addEventListener("offer") { (id, event, data) in
+            // Handling offers, if in list accept
+        }
+        
+        eventSource.addEventListener("answer") { (id, event, data) in
+            // Handling answers, if in list accept
+        }
+        
+        eventSource.addEventListener("ice-candidate") { (id, event, data) in
+            // Handling ice candidates, if in list accept
+        }
     }
 }
