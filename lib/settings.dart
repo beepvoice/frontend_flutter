aba8e6653de573dc19113d038285621cad6db8a3
Sudharshan <solderneer@gmail.com>
2019-07-27 12:12:27 +0800

    fix: moving all testing to the staging server

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,8 +1,8 @@
 // Overall sensible settings for development
 
-final String baseUrlCore = "http://localhost/core";
-final String baseUrlSignaling = "http://localhost/signal";
-final String baseUrlHeartbeat = "http://localhost/heartbeat";
-final String baseUrlPicture = "http://localhost/pictures";
-final String baseUrlLogin = "http://localhost:1837";
-// final String globalUserId = "u-fb91825f564a3cc110f11836fedea6f4";
+final String hostname = "https://staging.beepvoice.app";
+final String baseUrlCore = "$hostname/core";
+final String baseUrlSignaling = "$hostname/signal";
+final String baseUrlHeartbeat = "$hostname/heartbeat";
+final String baseUrlPicture = "$hostname/pictures";
+final String baseUrlLogin = "$hostname/login";
