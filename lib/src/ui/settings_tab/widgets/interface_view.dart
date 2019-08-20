8119de8afae74034b0c5070d947821576072c3e6
shihern <shihern2708@gmail.com>
2019-08-20 21:31:44 +0800

    feat: add basic interface options

--- lib/src/ui/settings_tab/widgets/interface_view.dart
+++ lib/src/ui/settings_tab/widgets/interface_view.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:frontend_flutter/src/ui/widgets/list_text_button.dart';
 
 import "../../widgets/top_bar.dart";
 import "../../widgets/list_button.dart";
@@ -11,8 +12,15 @@ class InterfaceView extends StatefulWidget {
 }
 
 class _InterfaceViewState extends State<InterfaceView> {
+  bool _enterToSend = false;
+
   @override
   Widget build(BuildContext context) {
+    var _buttonTextTheme = Theme.of(context)
+        .textTheme
+        .title
+        .copyWith(color: Theme.of(context).accentColor);
+
     return Column(
       children: <Widget>[
         TopBar(
@@ -22,7 +30,37 @@ class _InterfaceViewState extends State<InterfaceView> {
             Spacer(),
           ],
         ),
-        Text('Interface View'),
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.only(top: 0.0),
+            children: <Widget>[
+              SwitchListTile(
+                title: Text('Enter to Send', style: _buttonTextTheme),
+                value: _enterToSend,
+                onChanged: (bool value) {
+                  setState(() {
+                    _enterToSend = value;
+                  });
+                },
+              ),
+              ListTextButton(
+                text: 'Font Size',
+                subtitle: 'Medium',
+                textStyle: _buttonTextTheme,
+                onClickCallback: () {
+                  
+                },
+              ),
+              ListTextButton(
+                text: 'Wallpaper',
+                textStyle: _buttonTextTheme,
+                onClickCallback: () {
+                  
+                },
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
