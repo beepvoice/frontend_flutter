841b3f14381a665f1608e45f5d73cdd89f202ef2
shihern <shihern2708@gmail.com>
2019-08-24 17:27:06 +0800

    feat: add notification settings view

--- lib/src/ui/settings_tab/widgets/notifications_view.dart
+++ lib/src/ui/settings_tab/widgets/notifications_view.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:frontend_flutter/src/ui/widgets/list_text_button.dart';
 
 import "../../widgets/top_bar.dart";
-import "../../widgets/list_button.dart";
 
 class NotificationsView extends StatefulWidget {
   @override
@@ -11,8 +11,14 @@ class NotificationsView extends StatefulWidget {
 }
 
 class _NotificationsViewState extends State<NotificationsView> {
+  bool _inChatSounds = false;
+
   @override
   Widget build(BuildContext context) {
+    var _buttonTextTheme = Theme.of(context)
+        .textTheme
+        .title
+        .copyWith(color: Theme.of(context).accentColor);
     return Column(
       children: <Widget>[
         TopBar(
@@ -22,7 +28,38 @@ class _NotificationsViewState extends State<NotificationsView> {
             Spacer(),
           ],
         ),
-        Text('Notifications View'),
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.only(top: 0.0),
+            children: <Widget>[
+              SwitchListTile(
+                title: Text('In-Chat Sounds', style: _buttonTextTheme),
+                value: _inChatSounds,
+                onChanged: (bool value) {
+                  setState(() {
+                    _inChatSounds = value;
+                  });
+                },
+              ),
+              ListTextButton(
+                text: 'Notification Sound',
+                subtitle: 'Default',
+                textStyle: _buttonTextTheme,
+                onClickCallback: () {
+                  
+                },
+              ),
+              ListTextButton(
+                text: 'Vibrate',
+                subtitle: 'Default',
+                textStyle: _buttonTextTheme,
+                onClickCallback: () {
+                  
+                },
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
