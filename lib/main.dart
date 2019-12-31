7f07e4188a150844e8ef3be073d1da920c4f01a2
shihern <shihern2708@gmail.com>
2019-12-31 19:46:25 +0800

    fix: wait for WidgetsFlutterBinding to initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import "package:flutter/services.dart";
 import 'routes.dart';
+import 'package:flutter/widgets.dart';
 // import 'package:flutter/rendering.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
   Routes();
 }
